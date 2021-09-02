337aa546415a40bb7bb064b29f0ca856645ce1f2
tkeigwin <82317975+tkeigwin@users.noreply.github.com>
2021-09-02 19:27:40 +0800

    Added the JS to the JS repo

--- jspopupredundant.js
+++ jspopupredundant.js
@@ -1,1 +1,7 @@
-
+ function showAlert() {
+   alert ("Hello world!");
+ }
+  function showAlert() {
+    var myText = "This site is EXPEREMENTAL! Proceed at your own risk of your eyes being shot at by bad HTML";
+    alert (myText);
+  }
