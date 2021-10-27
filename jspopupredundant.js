5cb476b59a49ac3b4b369af559c78aa88a21d444
Tomas Keigwin <82317975+Iteration-Group@users.noreply.github.com>
2021-10-28 07:40:25 +0800

    Added the JS Popup from the new under construction file to the JS repo file.

--- jspopupredundant.js
+++ jspopupredundant.js
@@ -5,3 +5,7 @@
     var myText = "This site is EXPEREMENTAL! Proceed at your own risk of your eyes being shot at by bad HTML";
     alert (myText);
   }
+  function showAlert() {
+    var myText = "Under Construction. If you have access, please click the button and log your entry.";
+    alert (myText);
+  }
